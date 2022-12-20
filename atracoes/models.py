24cacdf6932e3ab1df8d71ef2613aaa106369475
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-20 17:32:49 -0300

    criando e modelando app atracoes

---
+++ atracoes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Recursos(models.Model):
+    nome = models.CharField(max_length=150, verbose_name='Recurso disponibilizado')
+    descricao = models.TextField(verbose_name='Descrição')
+    horario_func = models.TextField(verbose_name='Horário de funcionamento')
+    idade_minima = models.IntegerField()
+
+
+    def __str__(self):
+        return self.nome
--- core/models.py
+++ core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from atracoes.models import Recursos
 
 class PontosTuristicos(models.Model):
     nome = models.CharField(max_length=150)
-    descricao = models.TextField()
-    provado = models.BooleanField(default=False)
+    descricao = models.TextField(verbose_name='Descrição')
+    aprovado = models.BooleanField(default=False)
+    recurso = models.ManyToManyField(Recursos)
+
 
     def __str__(self):
         return self.nome

e4728d98724af5f157a05454a89fcd8acf60e431
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-27 15:45:39 -0300

    entendendo metodos update, partial_update, retrive de ModelViewSet

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -12,7 +12,7 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.all()
     
 
-    # def list(self, request, *args, **kwargs):
+    # def list(self, request, *args, **kwargs): # dispara no endpoint 
     #     return Response({'teste':'list sobrescrito'})
 
     
@@ -21,4 +21,16 @@ class PontoTuristicoViewSet(ModelViewSet):
 
 
     # def destroy(self, request, *args, **kwargs):
-    #     pass  # 
+    #     return super().destroy(request, *args, **kwargs)
+
+
+    def  retrieve(self, request, *args, **kwargs): # dispara num recurso do endpoint 
+        return super().retrieve(request, *args, **kwargs)
+
+
+    def update(self, request, *args, **kwargs): # atualiza um recurso/objeto inteiro
+        return Response({'teste':request})
+
+
+    def partial_update(self, request, *args, **kwargs): # atualiza parcialmente/partes de um recurso/objeto 
+        return super().partial_update(request, *args, **kwargs)

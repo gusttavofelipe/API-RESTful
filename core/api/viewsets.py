6f05bb4eb33e62f6a9fc3a5ad8ce3b55f15ec50f
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-28 17:16:57 -0300

    filtrando por query string

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,36 +10,27 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontosTuristicoSerializer
 
     def get_queryset(self):
-        return PontoTuristico.objects.all()
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+        
+        # filtrando por query string
+        if id:
+            queryset = queryset.filter(pk=id)
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+        if descricao:
+            queryset = queryset.filter(descricao__iexact=descricao)
+
+        return queryset
     
 
-    def list(self, request, *args, **kwargs): # dispara no endpoint 
-        return super().list(request, *args, **kwargs)
-
-    
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-
-
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
-
-
-    def  retrieve(self, request, *args, **kwargs): # dispara num recurso do endpoint 
-        return super().retrieve(request, *args, **kwargs)
-
-
-    def update(self, request, *args, **kwargs): # atualiza um recurso/objeto inteiro
-        return super().update(request, *args, **kwargs)
-
-
-    def partial_update(self, request, *args, **kwargs): # atualiza parcialmente/partes de um recurso/objeto 
-        return super().partial_update(request, *args, **kwargs)
-
-    
     # @action(methods=['get'], detail=True) 
     # def denounce(self, request, pk=None):
-    #     '''methods - em quais metodos disparar
-    #        detail - necessario ou não uma pk na ação da action '''
+    #     '''
+    #     methods - em quais metodos disparar
+    #     detail - necessario ou não uma pk na ação da action 
+    #     '''
     #     pass
 

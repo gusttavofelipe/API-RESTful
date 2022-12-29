bc320dffa3a6582829957b996320e36d58e9641c
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-29 08:06:53 -0300

    filtrando queryset com django_filters

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -2,6 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Recurso
 from .serializers import RecursoSerializer
 
+
+
 class RecursoViewSet(ModelViewSet):
     queryset = Recurso.objects.all()
     serializer_class = RecursoSerializer
+
+    # django_filters
+    filterset_fields = ('id', 'nome', 'descricao', )

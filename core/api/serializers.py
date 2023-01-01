bc5b5f9ef1581dec49a2bffcaa0a9087bf76c91e
Gustavo <gustavofeliperepositorio@gmail.com>
2023-01-01 15:40:00 -0300

    relacionamentos aninhados

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import RecursoSerializer
+from enderecos.api.serializers import EnderecoSerializer
+from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvalicaoSerializer
 
 class PontosTuristicoSerializer(ModelSerializer):
+    # relacioes aninhadas ()
+    # many=True apenas em relacionamentos ManyToMany
+    endereco = EnderecoSerializer()
+    recurso = RecursoSerializer(many=True) 
+    comentario = ComentarioSerializer(many=True)
+    avalicao = AvalicaoSerializer(many=True)
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'image')
+        fields = "__all__"
